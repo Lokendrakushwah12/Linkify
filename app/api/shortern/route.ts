2b6a344f6c56a72a743d81ff1951f3091e640bf1
Lokendrakushwah <Lokendrakushwah8051@gmail.com>
2024-10-08 01:04:07 +0530

    Build fix

--- app/api/shortern/route.ts
+++ app/api/shortern/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
 export async function POST(request: NextRequest) {
   const { url } = await request.json();
 
